3cfa09f808a82cf612508188e3b7f47b86738275
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-23 20:45:58 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -73,7 +73,7 @@ $("#signupForm").on("submit", function(event){
 
 function formIsValid(){
     var valid = true;
-    $("#username").change(function(){ alert("changed");});
+    $("#username").change(function(){});
     if ( !usernameAvailable ) {
         valid = false;
     }
