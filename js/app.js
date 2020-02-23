c142e8d7e5fb63356cd8891b8412fc7b9a538801
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-23 04:33:28 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 console.log("hello");
 
 $("#zip").on("change", function(){
-    alert($("#zip").val());
+    $.ajax({
+        method: "GET",
+        url: "https://cst336.herokuapp.com/projects/api/cityInfoAPI.php",
+        dataType: "json",
+        data: { "zip" : $("#zip").val()},
+        success: function(result, status){
+            alert(result);
+        }
+    });
 });
